7d51b8ee9cf5e802d3fc62956404b903101d54a3
Caroline de Jesus <161940709+carolinejesus@users.noreply.github.com>
2025-12-04 07:52:14 -0300

    Add SSL configuration to database connection

--- src/server.js
+++ src/server.js
@@ -19,6 +19,7 @@ const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    ssl: { rejecUnauthorized: false }
 });
 
 const app = express();
